9b1cc9090991c4ff4a0e77ef7b31994456ef35a9
Ethan-Gil <ethan.gil1@outlook.com>
2021-05-09 09:46:18 -0400

    Added temporary functionality to change password

---
+++ API/password.php
@@ -0,0 +1,18 @@
+<?php
+require(__DIR__.'/lib_db.php');
+require(__DIR__.'/lib_session.php');
+header('Content-type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    parse_str(file_get_contents("php://input"), $_PUT);
+    password($pdo, $_PUT);
+}
+
+function password ($pdo, $_PUT) {
+
+    $stmt = $pdo->prepare('UPDATE posts SET password=? WHERE posts.email =?');
+	$stmt->execute([password_hash($_PUT['password'], PASSWORD_DEFAULT), $_PUT['email']]);
+
+	die(json_encode(['status'=>1,'message'=>'Successfully changed password']));
+
+}
